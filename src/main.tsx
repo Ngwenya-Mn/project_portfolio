86a16e7926523a72ae43a33d86a68d027d033d48
unknown <milithevet@outlook.com>
2026-06-09 23:53:12 +0300

    redeployment of the project to GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
 import './index.css'
-import App from './App.tsx'
+import './App.css'
+import App from './App'
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+createRoot(document.getElementById('root')!).render(<App />)
